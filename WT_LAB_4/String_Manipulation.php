1bfcc6c8e72dc6d7c062af1e2074719d8c2605e5
Dinesh <34399732+dnshko@users.noreply.github.com>
2020-08-31 04:58:03 +0530

    Lab 4 add more fun()

--- WT_LAB_4/String_Manipulation.php
+++ WT_LAB_4/String_Manipulation.php
@@ -1,52 +1,57 @@
 <?PHP
 $strvalue =$_POST["strvalue"];
-
+$strvalue2 =$_POST["strvalue2"];
 $selected_radio = $_POST['str'];
 
 if ($selected_radio == 'Strlenfun') {
 echo Strlen($strvalue)  . "<br><br></hr>";
-echo <<<INFO
-    <div class='card text-white bg-primary mb-3' >       <div class='card-body'> <p class='card-text text-white'>PHP has a predefined function to get the length of a string. Strlen() displays the length of any string. It is more commonly used in validating input fields where the user is limited to enter a fixed length of characters.</p> </div></div>    
-    INFO;
+echo "<div class='card text-white bg-primary mb-3' >       <div class='card-body'> <p class='card-text text-white'>PHP has a predefined function to get the length of a string. Strlen() displays the length of any string. It is more commonly used in validating input fields where the user is limited to enter a fixed length of characters.</p> </div></div> "; 
+    
 }
 else if ($selected_radio == 'Str_word_count') {
 
     echo Str_word_count($strvalue) . "<br><br></hr>";
-    echo <<<INFO
-    <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Another function which enables display of the number of words in any specific string is str_word_count(). This function is  also useful in validation of input fields.</p> </div></div>    
-    INFO;
+    echo   "<div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Another function which enables display of the number of words in any specific string is str_word_count(). This function is  also useful in validation of input fields.</p> </div></div>  ";
 }
 else if ($selected_radio == 'Strev'){
 
     echo strrev($strvalue) . "<br><br></hr>";
-    echo <<<INFO
-    <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Strrev() is used for reversing a string. You can use this function to get the reverse version of any string.</p> </div></div>    
-    INFO;
+    echo " <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Strrev() is used for reversing a string. You can use this function to get the reverse version of any string.</p> </div></div> ";
 }
 else if ($selected_radio == 'Ucwords'){
 
     echo ucwords($strvalue) . "<br><br></hr>";
-    echo <<<INFO
-    <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Ucwords() is used to convert first alphabet of every word into uppercase.    </p> </div></div>    
-    INFO;        
+    echo "  <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Ucwords() is used to convert first alphabet of every word into uppercase.    </p> </div></div>    ";        
 }
 else if ($selected_radio == 'Stringtoupper'){
 
     echo strtoupper($strvalue) . "<br><br></hr>";
-    echo <<<INFO
-    <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Strtoupper() is used to convert a whole string into uppercase.    </p> </div></div>    
-    INFO;
+    echo "   <div class='card text-white bg-primary mb-3' >      <div class='card-body'> <p class='card-text text-white'>Strtoupper() is used to convert a whole string into uppercase.    </p> </div></div>  ";
 }
 else if ($selected_radio == 'Stringtolower'){
 
     echo strtolower($strvalue) . "<br><br></hr>";
-    echo <<<INFO
-    <div class='card text-white bg-primary mb-3' >   <div class='card-body'> <p class='card-text text-white'>Strtolower() is used to convert a string into lowercase.</p> </div></div>    
-    INFO;
+    echo "<div class='card text-white bg-primary mb-3' >   <div class='card-body'> <p class='card-text text-white'>Strtolower() is used to convert a string into lowercase.</p> </div></div> ";
+}
+elseif ($selected_radio == 'Strpos') {
+    echo Strpos("$strvalue","$strvalue2");
+    echo "<div class='card text-white bg-primary mb-3' >   <div class='card-body'> <p class='card-text text-white'>Strpos() enables searching particular text within a string. It works simply by matching the specific text in a string. If found, then it returns the specific position. If not found at all, then it will return “False”. Strops() is most commonly used in validating input fields like email.</p> </div></div> ";
+
+}
+elseif ($selected_radio == 'Str_repeat') {
+    echo Str_repeat("$strvalue","$strvalue2");
+    echo "<div class='card text-white bg-primary mb-3' >   <div class='card-body'> <p class='card-text text-white'>PHP provides a built-in function for repeating a string a specific number of times.</p> </div></div> ";
+
+}
+elseif ($selected_radio == 'Strcmp') {
+
+    echo Strcmp($strvalue,$strvalue2);
+    echo "<div class='card text-white bg-primary mb-3' >   <div class='card-body'> <p class='card-text text-white'>You can compare two strings by using strcmp(). It returns output either greater than zero, less than zero or equal to zero. If string 1 is greater than string 2 then it returns greater than zero. If string 1 is less than string 2 then it returns less than zero. It returns zero, if the strings are equal.</p> </div></div> ";
+
 }
 else {
     
-    echo "select any one";
+    echo "<h1 style='color:red;'>select any one<h1>";
 }
 
 ?>
